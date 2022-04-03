d8d428cd6f026e6aff5fb327c93be248ced9e758
Ramakant <gangwar.ramakant@gmail.com>
2022-04-04 02:07:21 +0530

    Update

--- database/seeders/MenuItemsTableSeeder.php
+++ database/seeders/MenuItemsTableSeeder.php
@@ -31,7 +31,7 @@ class MenuItemsTableSeeder extends Seeder
                 'icon_class' => 'voyager-bread voyager-bread-notification',
                 'color'      => null,
                 'parent_id'  => $parentMenuId,
-                'order'      => $maxOrder++,
+                'order'      => ++$maxOrder,
             ])->save();
         }
     }
